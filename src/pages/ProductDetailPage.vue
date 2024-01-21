05efc630e0a641e3186e868b86b0159c7bb32764
Zulfi Ismailov-Demiri <id@werkkasten.com>
2024-01-21 15:27:40 +0100

    build details page and cart page

--- src/pages/ProductDetailPage.vue
+++ src/pages/ProductDetailPage.vue
@@ -1,8 +1,28 @@
 <template>
-    <div>Detail Page</div>
+    <div class="max-w-4xl mx-auto px-4 xl:px-0 h-full">
+        <div class="">
+            <img :src="product.imageName" class="h-[44vh] mx-auto mb-12" />
+            <div class="w-1/2 mx-auto text-center">
+                <div class="w-full md:w.1/2">
+                    <h2 class="font-bold text-3xl">{{ product.name }}</h2>
+                    <p>{{ product.price }}</p>
+                </div>
+                <div class="mt-8">
+                    <button class="px-6 py-3 bg-black text-white rounded-full">Add to Cart</button>
+                </div>
+            </div>
+        </div> 
+    </div>
 </template>
 <script>
+import { products } from '../temp-data'
 export default {
     name: "ProducDetailtPage",
+    data() {
+        return {
+            // catch product id from
+            product: products.find(product => product.id === this.$route.params.productId),
+        }
+    }
 }
 </script>
--- src/pages/ShoppingCart.vue
+++ src/pages/ShoppingCart.vue
@@ -1,8 +1,30 @@
 <template>
-    <div>Shopping Cart</div>
+    <div class="max-w-4xl mx-auto px-4 xl:px-0 h-full">
+        <h1 class="font-bold text-[3em] mb-12">Products in Cart</h1>
+        <div class="flex flex-col">
+            <div class="grid grid-cols-2 p-4 border border-gray-100 rounded-xl shadow-xl"
+            v-for="item in cartItems"
+            :key="item.id"
+            >
+                <img :src="item.imageName" class="h-[20vh]" />
+                <div class="flex flex-frap mt-8">
+                    <div class="w-full md:w.1/2">
+                        <p>{{ item.name }}</p>
+                        <p>{{ item.price }}</p>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </div>
 </template>
 <script>
+import {cartItems} from '../temp-data'
 export default {
-    name: "ShopingCart",
+    name: "ShoppingCart",
+    data() {
+        return {
+            cartItems,
+        }
+    },
 }
 </script>
